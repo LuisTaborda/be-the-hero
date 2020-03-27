32ba72bc71adafd17679963fb7a0332be9e40165
LuisTaborda <luisgustavo.taborda@gmail.com>
2020-03-27 15:26:55 -0300

    correção ortográfica

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -58,7 +58,7 @@ export default function Incidents(){
             </View>
 
             <Text style={styles.title}>Bem-vindo!</Text>
-            <Text style={styles.description}>Escolha um dos casos abaixoe e salve o dia.</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
             <FlatList 
             data={incidents}
